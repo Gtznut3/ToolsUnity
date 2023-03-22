7d781696b09d0f9dfffcde2303eed05cd828671a
Gaëtan Peronnet <gaetan.peronnet@gmail.com>
2023-03-22 15:37:44 +0100

    Task 3 finished

--- Assets/Scripts/Recoltable.cs
+++ Assets/Scripts/Recoltable.cs
@@ -21,7 +21,7 @@ public class Recoltable : MonoBehaviour
             manager.RemoveRecoltable(this);
             Destroy(gameObject);
         }
-    }
+    }   
 
     private void OnDrawGizmos()
     {
